c3f1e6dd9cda17a844e05bffe4c04972967f72b6
worlfee <worlfee@gmail.com>
2019-05-15 00:03:24 +0300

    app constructor

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+
+
+app.get('/', function(req, res){
+
+    res.send('listening..');
+    
+
+
+});
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
