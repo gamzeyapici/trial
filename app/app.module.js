52a9f0ca3c46df12f8d4208ebaae4eacfd91c2d1
gamze <worlfee@gmail.com>
2019-05-16 16:10:15 +0300

    home & login component created

--- app/app.module.js
+++ app/app.module.js
@@ -4,13 +4,29 @@
         'ui.router',
         'ngResource'
     ])
-    .run(function() {})
-    .component('product',{
-        template:"<div>{{product}}</div>, <div>product</div>",
-        controller: function ($scope){
-            debugger;
-            $scope.product="productComponentElement";
-            debugger;
-        }
-    })
+        .run(function () { })
+        .component('product', {
+            template: "<div>{{product}}</div>, <div>product</div>",
+            controller: function ($scope) {
+                debugger;
+                $scope.product = "productComponentElement";
+                debugger;
+            }
+        })
+        .component('login', {
+            template: "<div>{{login}}</div>, <div>login</div>",
+            controller: function ($scope) {
+                debugger;
+                $scope.login = "loginComponentElement";
+                debugger;
+            }
+        })
+        .component('home', {
+            template: "<div>{{home}}</div>, <div>home</div>",
+            controller: function ($scope) {
+                debugger;
+                $scope.home = "homeComponentElement";
+                debugger;
+            }
+        })
 })();
