9707a46bf9dee55b3c4ee3e9cf2cfbf1466ecf2e
gamze <worlfee@gmail.com>
2019-05-16 15:38:17 +0300

    index html created

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.get('/', function(req, res){
-    res.send('listening..');
-});
+// app.get('/', function(req, res){
+//     res.send('listening..');
+// });
+
+
+
+app.use(express.static(path.resolve(`${__dirname}`)));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(`${__dirname}/index.html`)));
 
 
 
